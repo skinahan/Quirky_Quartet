8db745a70f3874616c4020799eb50d4e00d31170
LSaldyt <lucassaldyt@gmail.com>
2022-11-16 21:34:18 -0700

    Output to CSV

--- scripts/generate_synthetic_data.py
+++ scripts/generate_synthetic_data.py
@@ -1,8 +1,17 @@
 from library.openai_synthetic_dataset import generate
+import csv
+from pathlib import Path
+
+headers = ['id', 'description', 'code', 'outputs']
 
 def run():
-    for depth in range(1, 6):
+    for depth in range(1, 3):
         count = 0
-        for program in generate(depth=depth):
-            count += 1
-        print(f'A depth of {depth} gives {count} programs (including duplicates)')
+        with open(f'data/synthetic_depth_{depth}.csv', 'w') as outfile:
+            writer = csv.writer(outfile)
+            writer.writerow(headers)
+            for program in generate(depth=depth):
+                count += 1
+                print(program)
+                writer.writerow([count, program.description, '', ''])
+            print(f'A depth of {depth} gives {count} programs (including duplicates)')
